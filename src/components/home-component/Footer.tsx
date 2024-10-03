337532031e151a9aa3db1b1be0f8343a038603d5
ducanh <anh.tranduc@mobifone.vn>
2024-10-04 00:36:11 +0700

    homepage

---
+++ src/components/home-component/Footer.tsx
@@ -0,0 +1,43 @@
+export default function Footer() {
+    return <div className="h-[665px] bg-[rgba(18,18,18,1)] pt-[40px] pb-[40px] pl-[67px] pr-[67px]">
+        <div className="flex justify-between border-b-[rgb(66, 66, 66)] border-b-[1px] pb-[200px]">
+            <div>
+                <button className="text-[rgba(255,255,255,0.9)] mb-[20px]">Content</button>
+                <ul className="space-y-[10px]">
+                    <li><a className="text-[rgba(119,119,119,0.9)]">New assets</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">The most popular content</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">Search trend</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">Blog</a></li>
+                </ul>
+            </div>
+            <div>
+                <button className="text-[rgba(255,255,255,0.9)] mb-[20px]">Information</button>
+                <ul className="space-y-[10px]">
+                    <li><a className="text-[rgba(119,119,119,0.9)]">Pricing</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">About us</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">API</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">Jobs</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">Sell content</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">Events</a></li>
+                </ul>
+            </div>
+            <div>
+                <button className="text-[rgba(255,255,255,0.9)] mb-[20px]">Legal</button>
+                <ul className="space-y-[10px]">
+                    <li><a className="text-[rgba(119,119,119,0.9)]">Terms of use</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">License agreement</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">Privacy Policy</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">Jobs</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">Sell content</a></li>
+                    <li><a className="text-[rgba(119,119,119,0.9)]">Events</a></li>
+                </ul>
+            </div>
+            <div>
+                <button className="text-[rgba(255,255,255,0.9)]">SOCIAL MEDIA</button>
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </div>
+}
--- src/components/home-component/HomePage.tsx
+++ src/components/home-component/HomePage.tsx
@@ -5,6 +5,7 @@ import HomeDropdown from './HomeDropdown'
 import { Search } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
+import Footer from './Footer'
 export default function HomePage() {
     return (
         <div className='flex flex-col relative'>
@@ -49,14 +50,14 @@ export default function HomePage() {
                             </div>
                         </Link>
 
-                        {/* <Link href={"/"}>
+                        <Link href={"/tools-page"}>
                             <div className='relative flex flex-col h-56 w-56 bg-red-400 rounded-xl overflow-hidden group'>
                                 <div className='absolute top-3 left-2 z-10 text-white font-bold'>Designer</div>
                                 <span className='absolute top-10 left-2 z-10 text-white text-xs font-bold opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100'>
-                                    Create images from worlds in real time
+                                    Edit template from your browser
                                 </span>
                                 <Image
-                                    src="/image/default/designerImg.jpg"
+                                    src="/image/default/designer-bg.jpg"
                                     alt='Image'
                                     width={300}
                                     height={300}
@@ -64,11 +65,92 @@ export default function HomePage() {
                                 />
                                 <div className='absolute inset-0 bg-black opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-50' />
                             </div>
-                        </Link> */}
+                        </Link>
+
+                        <Link href={"/tools-page"}>
+                            <div className='relative flex flex-col h-56 w-56 bg-red-400 rounded-xl overflow-hidden group'>
+                                <div className='absolute top-3 left-2 z-10 text-white font-bold'>Mockup generator</div>
+                                <span className='absolute top-10 left-2 z-10 text-white text-xs font-bold opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100'>
+                                    Mockup generator
+                                </span>
+                                <Image
+                                    src="/image/default/mockup-bg.jpg"
+                                    alt='Image'
+                                    width={300}
+                                    height={300}
+                                    className="transition-transform duration-300 ease-in-out group-hover:scale-110"
+                                />
+                                <div className='absolute inset-0 bg-black opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-50' />
+                            </div>
+                        </Link>
+
+                        <Link href={"/tools-page"}>
+                            <div className='relative flex flex-col h-56 w-56 bg-red-400 rounded-xl overflow-hidden group'>
+                                <div className='absolute top-3 left-2 z-10 text-white font-bold'>Reimagine</div>
+                                <span className='absolute top-10 left-2 z-10 text-white text-xs font-bold opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100'>
+                                    Reimagine
+                                </span>
+                                <Image
+                                    src="/image/default/reimagine-bg.jpg"
+                                    alt='Image'
+                                    width={300}
+                                    height={300}
+                                    className="transition-transform duration-300 ease-in-out group-hover:scale-110"
+                                />
+                                <div className='absolute inset-0 bg-black opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-50' />
+                            </div>
+                        </Link>
+
+                        <Link href={"/tools-page"}>
+                            <div className='relative flex flex-col h-56 w-56 bg-red-400 rounded-xl overflow-hidden group'>
+                                <div className='absolute top-3 left-2 z-10 text-white font-bold'>Retouch</div>
+                                <span className='absolute top-10 left-2 z-10 text-white text-xs font-bold opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100'>
+                                    Retouch
+                                </span>
+                                <Image
+                                    src="/image/default/retouch.jpg"
+                                    alt='Image'
+                                    width={300}
+                                    height={300}
+                                    className="transition-transform duration-300 ease-in-out group-hover:scale-110"
+                                />
+                                <div className='absolute inset-0 bg-black opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-50' />
+                            </div>
+                        </Link>
 
                     </div>
                 </div>
             </div>
+            <div>
+                <div>
+                    <h4 className='text-[rgb(51,106,234)] text-[15px] text-center'>BENFITS</h4>
+                    <h2 className='text-[56px] text-center'>Let’s make your ideas break through</h2>
+                    <p className='text-[15px] font-semibold text-center'>Find the most up-to-date vocabulary of images, videos, signs, symbols, and fonts</p>
+                </div>
+                <div className='mt-[100px]'>
+                    <div className=''>
+                        <video src="https://fps.cdnpk.net/home/benefits/benefit-quality.mp4" width="70" height="70" class="_1286nb14ku _1286nb14m6 _1286nb14ni _1286nb14ou _1286nb196" autoplay="" loop="" muted="" playsinline="" disableremoteplayback=""></video>                        
+                        <h3 className='text-[32px]'>Best quality or nothing</h3>
+                        <p>Download scroll-stopping images of the highest quality to make professional designs.</p>
+                    </div>
+                    <div>
+                        <video src="https://fps.cdnpk.net/home/benefits/benefit-ready.mp4" width="70" height="70" class="_1286nb14ku _1286nb14m6 _1286nb14ni _1286nb14ou _1286nb196" autoplay="" loop="" muted="" playsinline="" disableremoteplayback=""></video>
+                        <h3 className='text-[32px]'>Ready-to-use everything</h3>
+                        <p>Download scroll-stopping images of the highest quality to make professional designs.</p>
+                    </div>
+                    <div>
+                        <video src="https://fps.cdnpk.net/home/benefits/benefit-content.mp4" width="70" height="70" class="_1286nb14ku _1286nb14m6 _1286nb14ni _1286nb14ou _1286nb196" autoplay="" loop="" muted="" playsinline="" disableremoteplayback=""></video>                        
+                        <h3 className='text-[32px]'>Fresh content every day</h3>
+                        <p>Our library is updated on a daily basis so you can find the newest and trendiest photos and designs.</p>
+                    </div>
+                    <div>
+                        <video src="https://fps.cdnpk.net/home/benefits/benefit-think.mp4" width="70" height="70" class="_1286nb14ku _1286nb14m6 _1286nb14ni _1286nb14ou _1286nb196" autoplay="" loop="" muted="" playsinline="" disableremoteplayback=""></video>                        
+                        <h3 className='text-[32px]'>If you can think of it, you can find it</h3>
+                        <p>Guaranteed search results: there’s an image and style for every project you might think of.</p>
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </div>
     )
 }
