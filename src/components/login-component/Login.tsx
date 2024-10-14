d5b44c6d915bfabebb006868bffbd8edb848a2be
DacVuCaMap <namvu04112k@gmail.com>
2024-10-14 22:31:27 +0700

    up

--- src/components/login-component/Login.tsx
+++ src/components/login-component/Login.tsx
@@ -33,7 +33,7 @@ const Login = () => {
   const handleLogin = async (data: TypeLogin) => {
     try {
       const res: AxiosResponse<TypeResponse> = await authLogin(data);
-      const { status, message: resMessage, jwt, id, expired,role } = res.data;
+      const { status, message: resMessage, jwt, id, expired, role } = res.data;
       if (status === 0) {
         message.error(resMessage);
         return;
@@ -65,114 +65,112 @@ const Login = () => {
         });
       }
       // router.push("/");
-      window.location.href="/";
+      window.location.href = "/";
     } catch (error: any) {
       message.error("error", error?.data?.message);
     }
   };
   return (
-    <div className="flex h-screen relative">
+    <div className="flex flex-row h-screen relative">
       <div className="w-full h-screen">
         {backgroundImage && (
           <Image
             src={`/login/${backgroundImage}`}
-            width={1440}
-            height={900}
+            width={1000}
+            height={0}
             alt="Image"
             objectFit="cover"
             priority
-            quality={75}
+            quality={100}
             className="w-full h-full object-cover"
           />
         )}
       </div>
-      <div className="absolute right-0 bg-white px-10 py-10 h-screen overflow-auto">
+      <div className="bg-white px-16 w-[450px] py-10 h-screen overflow-auto">
         {isLogin && (
-          <div className="w-3/12">
-            <div className="w-80 pt-16 flex flex-col gap-7 ">
-              <div className="flex justify-center">
-                <Image
-                  src="/image/logo-font.png"
-                  alt="Logo"
-                  width={200}
-                  height={0}
-                  className="object-contain"
-                />
-              </div>
-              <p className="text-center mt-3 text-xl text-gray-700 font-semibold">
-                Login
-              </p>
-              <div className="flex justify-center">
-                <Form
-                  form={form}
-                  name="basic"
-                  labelCol={{ span: 8 }}
-                  wrapperCol={{ span: 16 }}
-                  initialValues={{ remember: true }}
-                  autoComplete="off"
-                  onFinish={(values) => handleLogin(values)}
-                  className="w-9/12"
-                >
-                  <label className="text-sm font-medium	">
-                    Email<span className="text-red-500">*</span>
-                  </label>
-                  <Form.Item<FieldType>
-                    name="email"
-                    rules={[
-                      { required: true, message: "Please input your email!" },
-                      {
-                        type: "email",
-                        message: "Invalid email format!",
-                      },
-                    ]}
-                  >
-                    <Input className="w-60 rounded-md h-9 border-slate-300" />
-                  </Form.Item>
-                  <label className="text-sm font-medium">
-                    Password<span className="text-red-500">*</span>
-                  </label>
-                  <Form.Item<FieldType>
-                    name="password"
-                    rules={[
-                      {
-                        required: true,
-                        message: "Please input your password!",
-                      },
-                    ]}
-                  >
-                    <Input.Password className="w-60 h-9" />
-                  </Form.Item>
+          <div className="pt-16 flex flex-col gap-7 ">
+            <div className="flex flex-col justify-center items-center">
+              <Image
+                src="/image/logo-font.png"
+                alt="Logo"
+                width={180}
+                height={0}
+                className=" object-contain"
+              />
+            </div>
+            <p className="text-center mt-3 text-lg text-gray-700 font-semibold">
+              Login
+            </p>
+            <div className="flex justify-center">
+              <Form
+                form={form}
+                name="basic"
 
-                  <Form.Item<FieldType>
-                    name="isRemember"
-                    valuePropName="checked"
-                  >
-                    <Checkbox>Stay logged in</Checkbox>
-                  </Form.Item>
-
-                  <Form.Item>
-                    <Button
-                      htmlType="submit"
-                      type="primary"
-                      className="w-60 h-10 rounded text-base font-medium"
-                    >
-                      Log in
-                    </Button>
-                  </Form.Item>
-                </Form>
-              </div>
-              <div className="font-semibold flex gap-1 justify-center text-sm text-gray-600">
-                <span className="">Don’t you have an account?</span>
-                <span
-                  className="text-blue-700 cursor-pointer"
-                  onClick={() => {
-                    setIsRegister(true);
-                    setIsLogin(false);
-                  }}
+                initialValues={{ remember: true }}
+                autoComplete="off"
+                onFinish={(values) => handleLogin(values)}
+                className="flex flex-col w-full"
+              >
+                <label className="text-sm font-medium	">
+                  Email<span className="text-red-500">*</span>
+                </label>
+                <Form.Item<FieldType>
+                  name="email"
+                  rules={[
+                    { required: true, message: "Please input your email!" },
+                    {
+                      type: "email",
+                      message: "Invalid email format!",
+                    },
+                  ]}
+                  className="w-full"
                 >
-                  Sign up
-                </span>
-              </div>
+                  <Input className="w-full rounded-md h-9 border-slate-300" />
+                </Form.Item>
+                <label className="text-sm font-medium">
+                  Password<span className="text-red-500">*</span>
+                </label>
+                <Form.Item<FieldType>
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password!",
+                    },
+                  ]}
+                >
+                  <Input.Password className="w-full h-9" />
+                </Form.Item>
+
+                <Form.Item<FieldType>
+                  name="isRemember"
+                  valuePropName="checked"
+                >
+                  <Checkbox>Stay logged in</Checkbox>
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    htmlType="submit"
+                    type="primary"
+                    className="w-full h-10 rounded text-base font-medium"
+                  >
+                    Log in
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
+            <div className=" flex flex-row w-full text-sm justify-center space-x-2">
+              <span className="">Don’t you have an account?</span>
+              <span
+                className="text-blue-700 cursor-pointer"
+                onClick={() => {
+                  setIsRegister(true);
+                  setIsLogin(false);
+                }}
+              >
+                Sign up
+              </span>
             </div>
           </div>
         )}
--- src/ultils/index.ts
+++ src/ultils/index.ts
@@ -22,7 +22,7 @@ export const setCookie = (
 export const deleteAllCookies = () =>  {
   const cookies = document.cookie.split(";");
 
-  for (let cookie of cookies) {
+  for (const cookie of cookies) {
     const eqPos = cookie.indexOf("=");
     const name = eqPos > -1 ? cookie.substr(0, eqPos).trim() : cookie.trim();
     document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
