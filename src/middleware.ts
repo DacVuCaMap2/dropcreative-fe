01ed675a6c26f0296eb9bfa1e1008eb61b7a5925
DacVuCaMap <namvu04112k@gmail.com>
2024-10-14 16:15:50 +0700

    up

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,19 @@
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-    const isLogin = request.cookies.get('account_id');
+    const cookie = cookies();
+    const loggedIn = cookie.get('account_id')?.value
 
-    if (!isLogin) {
-        if (request.nextUrl.pathname.includes('/admin') || request.nextUrl.pathname.includes('/tools-page')  ) {
-            return NextResponse.redirect(new URL('/login', request.url));
-        }
+    console.log(loggedIn);
+    if (!loggedIn) {
+        return NextResponse.redirect(new URL('/login', request.url));
     }
 
-    // Nếu có cookie hoặc không phải trang admin, cho phép truy cập
     return NextResponse.next();
 }
 
 // Cấu hình matcher để áp dụng middleware cho các trang admin
 export const config = {
-    matcher: '',
+    matcher: ['/admin/:path*','/tools-page/:path*'],
 };
