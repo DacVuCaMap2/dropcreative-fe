001370093a9cd259159a9cb202982216bb4aafac
DacVuCaMap <namvu04112k@gmail.com>
2024-10-14 19:16:09 +0700

    up

--- src/components/admin-component/product-component/AllProductComponent.tsx
+++ src/components/admin-component/product-component/AllProductComponent.tsx
@@ -8,16 +8,16 @@ type Props = {
     roleStr: any
 }
 export default function AllProductComponent(props: Props) {
-    const roleUser = generalRoles[0];
+    const roleUser = generalRoles[1];
     return (
         <div className='w-full flex flex-col space-y-4'>
             <div className='flex justify-end w-full'>
                 {(props.roleStr && props.roleStr === roleUser) ?
+                    ''
+                    :
                     <Link href={'/admin/all-product/add'} className='flex flex-row items-center bg-blue-500 font-bold text-white p-2 rounded shadow hover:bg-blue-600 transition duration-300'>
                         <Plus className='mr-2' /> <span>Add Product</span>
                     </Link>
-                    :
-                    ''
                 }
             </div>
             <ListProduct listProduct={props.listProduct} />
