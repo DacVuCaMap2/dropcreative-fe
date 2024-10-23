4da712a32e0abc6bb03819f98869725604c051ae
DacVuCaMap <namvu04112k@gmail.com>
2024-10-24 04:52:32 +0700

    up

--- src/components/admin-component/product-component/CardProductItem.tsx
+++ src/components/admin-component/product-component/CardProductItem.tsx
@@ -23,7 +23,7 @@ export function CardProductItem(props: Props) {
     }
   }
   return (
-    <div className="shadow-lg transition-transform trasistion border border-neutral-300 text-neutral-600 md:h-[500px] h-[700px] flex flex-col hover:shadow-2xl duration-300 ease-in-out overflow-hidden hover:bg-gray-100">
+    <div className="shadow-lg transition-transform trasistion border border-neutral-300 text-neutral-600  h-[570px] flex flex-col hover:shadow-2xl duration-300 ease-in-out overflow-hidden hover:bg-gray-100">
       <div className="h-80 overflow-hidden flex items-center justify-center mb-2">
         <div className="relative h-full w-full group flex justify-center items-center">
           <Link href={`/landing-page/product/${data.id}`} target="_blank">
@@ -66,31 +66,39 @@ export function CardProductItem(props: Props) {
         <div className="flex flex-col items-center justify-center pt-2 text-xs text-neutral-500 space-y-2">
           {props.isAdmin &&
             <div className="flex flex-row justify-between items-center px-4 w-full">
-              <p className="w-1/2 font-bold">Creator:</p>
-              <p className="w-1/2 text-center">{data.productCreator.email}</p>
+              <p className="w-full font-bold">Creator:</p>
+              <p className="w-2/3 text-left truncate">{data.productCreator.email}</p>
             </div>
           }
           <div className="flex flex-row justify-between items-center px-4 w-full">
-            <p className="w-1/2 font-bold">License:</p>
-            <p className="w-1/2 text-center">{data.serviceType}</p>
+            <p className="w-full font-bold">License:</p>
+            <p className="w-2/3 text-left">{data.serviceType}</p>
           </div>
           <div className="flex flex-row justify-between items-center px-4 w-full">
-            <p className="w-1/2 font-bold">Price:</p>
-            <p className="w-1/2 text-center">$ {data.price}</p>
+            <p className="w-full font-bold">CR:</p>
+            <p className="w-2/3 text-left">{data.cr}%</p>
           </div>
           <div className="flex flex-row justify-between items-center px-4 w-full">
-            <p className="w-1/2 font-bold">Compare Price:</p>
-            <p className="w-1/2 text-center">$ {data.comparePrice}</p>
+            <p className="w-full font-bold">AOV:</p>
+            <p className="w-2/3 text-left">{data.aov}</p>
           </div>
           <div className="flex flex-row justify-between items-center px-4 w-full">
-            <p className="w-1/2 font-bold">Cost per Price:</p>
-            <p className="w-1/2 text-center">$ {data.costPerPrice}</p>
+            <p className="w-full font-bold">Price:</p>
+            <p className="w-2/3 text-left">${data.price}</p>
           </div>
           <div className="flex flex-row justify-between items-center px-4 w-full">
-            <p className="w-1/2 font-bold">Shipping Fee:</p>
-            <p className="w-1/2 text-center">$ {data.shippingFee}</p>
+            <p className="w-full font-bold">Compare Price:</p>
+            <p className="w-2/3 text-left">${data.comparePrice}</p>
           </div>
-          
+          <div className="flex flex-row justify-between items-center px-4 w-full">
+            <p className="w-full font-bold">Cost per Price:</p>
+            <p className="w-2/3 text-left">${data.costPerPrice}</p>
+          </div>
+          <div className="flex flex-row justify-between items-center px-4 w-full">
+            <p className="w-full font-bold">Shipping Fee:</p>
+            <p className="w-2/3 text-left">${data.shippingFee}</p>
+          </div>
+
         </div>
       </div>
     </div>
