37254781627d737e20111459d349f819f260baca
cong.vu <cong.vu@ntq-solution.com.vn>
2024-10-23 22:22:23 +0700

    feat: ui

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,30 +1,31 @@
-
-import HomeHeadNavBar from '@/components/home-component/HomeHeadNavBar';
-import { cookies } from 'next/headers';
-import React from 'react';
+import Footer from "@/components/common-component/Footer";
+import HomeHeadNavBar from "@/components/home-component/HomeHeadNavBar";
+import { cookies } from "next/headers";
+import React from "react";
 
 const Layout = ({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) => {
-    const cookie = cookies();
-    const emailCookie = cookie.get("email");
-    const roleCookie = cookie.get("role");
-    let email = "";
-    let role = ""
-    if (emailCookie && roleCookie) {
-        email = emailCookie.value;
-        role = roleCookie.value;
-    }
-    return (
-        <div>
-            <main className=" w-full relative">
-                <HomeHeadNavBar email={email} role={role} type={0}/>
-                {children}
-            </main>
-        </div>
-    );
+  const cookie = cookies();
+  const emailCookie = cookie.get("email");
+  const roleCookie = cookie.get("role");
+  let email = "";
+  let role = "";
+  if (emailCookie && roleCookie) {
+    email = emailCookie.value;
+    role = roleCookie.value;
+  }
+  return (
+    <div>
+      <main className=" w-full relative">
+        <HomeHeadNavBar email={email} role={role} type={0} />
+        {children}
+        <Footer background="bg-black" textColor="text-white" />
+      </main>
+    </div>
+  );
 };
 
 export default Layout;
